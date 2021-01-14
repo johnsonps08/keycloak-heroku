097c466571abd5ce1e23403d7b914956f842f319
Patrick S Johnson <johnsonps08@gmail.com>
2021-01-13 21:58:29 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# Set database config from Heroku DATABASE_URL
-if [ "$DATABASE_URL" != "" ]; then
-    echo "Found database configuration in DATABASE_URL=$DATABASE_URL"
-
-    regex='^postgres://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
-    if [[ $DATABASE_URL =~ $regex ]]; then
-        export DB_ADDR=${BASH_REMATCH[3]}
-        export DB_PORT=${BASH_REMATCH[4]}
-        export DB_DATABASE=${BASH_REMATCH[5]}
-        export DB_USER=${BASH_REMATCH[1]}
-        export DB_PASSWORD=${BASH_REMATCH[2]}
-
-        echo "DB_ADDR=$DB_ADDR, DB_PORT=$DB_PORT, DB_DATABASE=$DB_DATABASE, DB_USER=$DB_USER, DB_PASSWORD=$DB_PASSWORD"
-        export DB_VENDOR=postgres
-    fi
-
-fi
-
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
@@ -39,6 +21,25 @@ file_env() {
 	export "$var"="$val"
 	unset "$fileVar"
 }
+
+file_env 'DATABASE_URL'
+
+# Set database config from Heroku DATABASE_URL
+if [ $DATABASE_URL != "" ]; then
+
+    regex='^postgres://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
+    if [[ $DATABASE_URL =~ $regex ]]; then
+        export DB_ADDR=${BASH_REMATCH[3]}
+        export DB_PORT=${BASH_REMATCH[4]}
+        export DB_DATABASE=${BASH_REMATCH[5]}
+        export DB_USER=${BASH_REMATCH[1]}
+        export DB_PASSWORD=${BASH_REMATCH[2]}
+
+        echo "DB_ADDR=$DB_ADDR, DB_PORT=$DB_PORT, DB_DATABASE=$DB_DATABASE, DB_USER=$DB_USER, DB_PASSWORD=$DB_PASSWORD"
+        export DB_VENDOR=postgres
+    fi
+
+fi
 
 ##################
 # Add admin user #
